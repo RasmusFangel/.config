cfac02bed9421a40ab10440d29d57d3a58caabf6
Rasmus Hansen <r.fangelhansen@gmail.com>
2024-12-20 14:24:33 +0000

    best conf to date

--- Lazy/lua/config/autocmds.lua
+++ Lazy/lua/config/autocmds.lua
@@ -9,7 +9,7 @@ local FILES_TO_LOOK_FOR = {
   },
   ["package.json"] = {
     lang = "javascript",
-    venv_dir = ".node_modules",
+    venv_dir = "node_modules",
     icon = "",
   },
   ["rust_at_some_point"] = {
@@ -46,6 +46,63 @@ local function get_venv_info()
   }
 end
 
+function CheckForPytestCov()
+  -- Assume the file we want to check is named 'requirements.txt'
+  local file_path = vim.fn.getcwd() .. "/pyproject.toml"
+
+  -- Check if the file exists
+  if vim.fn.filereadable(file_path) == 1 then
+    -- Read the file content
+    local content = vim.fn.readfile(file_path)
+
+    -- Join all lines into a single string for easier searching
+    local full_content = table.concat(content, "\n")
+
+    -- Check if 'pytest-cov' is in the content
+    if string.find(full_content, "pytest%-cov") then
+      return true
+      -- Add your desired action here
+    else
+      return false
+      -- Add your desired action here
+    end
+  else
+    print("requirements.txt not found in the current working directory")
+  end
+end
+
+local function set_neotest(python_path_abs)
+  local args = {}
+  if CheckForPytestCov() == true then
+    args = { "--log-level", "DEBUG", "--no-cov" }
+  else
+    args = { "--log-level", "DEBUG" }
+  end
+
+  require("neotest").setup({
+    adapters = {
+      require("neotest-python")({
+        -- Extra arguments for nvim-dap configuration
+        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+        dap = { justMyCode = false },
+        -- Command line arguments for runner
+        -- Can also be a function to return dynamic values
+        args = args,
+        -- Runner to use. Will use pytest if available by default.
+        -- Can be a function to return dynamic value.
+        runner = "pytest",
+        pytest = python_path_abs .. " -m pytest",
+        -- Custom python path for the runner.
+        -- Can be a string or a list of strings.
+        -- Can also be a function to return dynamic value.
+        -- If not provided, the path will be inferred by checking for
+        -- virtual envs in the local directory and for Pipenev/Poetry configs
+        python = python_path_abs,
+      }),
+    },
+  })
+end
+
 local function refresh_lspconfig(env_info)
   local lsp_config = require("lspconfig")
   local python_path_abs = path.join(env_info.path, env_info.venv_dir, "bin", "python")
@@ -64,6 +121,7 @@ local function refresh_lspconfig(env_info)
         pythonPath = python_path_abs,
         analysis = {
           typeCheckingMode = "strict",
+          reportUnusedVariable = "off",
           -- Additional Pyright configuration
         },
       },
@@ -79,22 +137,7 @@ local function refresh_lspconfig(env_info)
       vim.keymap.set("n", "<F9>", dap.restart, { buffer = bufnr })
       vim.keymap.set("n", "<F10>", dap.close, { buffer = bufnr })
 
-      -- Setup Debug Conf
-      local debug_conf_abs = path.join(env_info.path, ".vscode", "launch.json")
-
-      local launchjs_check_mark = ""
-      -- Check if the file exists and is readable
-      if vim.fn.filereadable(debug_conf_abs) == 1 then
-        require("dap.ext.vscode").load_launchjs(debug_conf_abs)
-        launchjs_check_mark = "✓ "
-      end
-
-      -- Set Lualine
-      local ll_string = env_info.project .. " | " .. launchjs_check_mark .. env_info.venv_dir
-
-      vim.g.LL_VENV_STRING = ll_string
-
-      require("lualine").refresh()
+      set_neotest(python_path_abs)
     end,
   })
 
@@ -108,6 +151,25 @@ local LAST_ACTIVATED_ENV = ""
 vim.g.LL_VENV_STRING = ""
 local start_path = vim.fn.getenv("PATH")
 
+local function load_debug_and_lualine(env_info)
+  -- Setup Debug Conf
+  local debug_conf_abs = path.join(env_info.path, ".vscode", "launch.json")
+
+  local launchjs_check_mark = ""
+  -- Check if the file exists and is readable
+  if vim.fn.filereadable(debug_conf_abs) == 1 then
+    require("dap.ext.vscode").load_launchjs(debug_conf_abs)
+    launchjs_check_mark = "✓ "
+  end
+
+  -- Set Lualine
+  local ll_string = env_info.project .. " | " .. launchjs_check_mark .. env_info.venv_dir
+
+  vim.g.LL_VENV_STRING = ll_string
+
+  require("lualine").refresh()
+end
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   desc = "Dynamically update LSP and environment",
   pattern = "*",
@@ -117,7 +179,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     if env_info.lang ~= "" then
       local venv_abs = path.join(env_info.path, env_info.venv_dir)
 
-      if vim.fn.isdirectory(venv_abs) == 0 then
+      if vim.fn.isdirectory(venv_abs) == 0 and env_info.venv_dir ~= "" then
         print("No venv found! Please build.")
         return
       end
@@ -129,12 +191,14 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
         local new_path = path.join(venv_abs, "bin") .. ":" .. start_path
         vim.fn.setenv("PATH", new_path)
 
-        refresh_lspconfig(env_info)
+        if env_info.lang == "python" then
+          refresh_lspconfig(env_info)
+        end
+
+        load_debug_and_lualine(env_info)
 
         vim.cmd("cd " .. env_info.path)
       end
     end
   end,
 })
-
--- Rest of your configuration remains the same...
---
+++ Lazy/lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    direction = "float",
+  },
+  keys = {
+    { "<leader>T", "<cmd>ToggleTerm<cr>", "toggle floating terminal" },
+  },
+}
